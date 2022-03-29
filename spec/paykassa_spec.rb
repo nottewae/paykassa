7a02a38754389b358042e0fb42e0b0677180ca06
nottewae <nottewae@gmail.com>
2022-03-29 09:10:25 +0300

    first commit

---
+++ spec/paykassa_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Paykassa do
+  it "has a version number" do
+    expect(Paykassa::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
